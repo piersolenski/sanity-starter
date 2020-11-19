372be3e7d2ef13c6a1ae9488956f89bc9a736351
Piers Olenski <hello@piers.io>
2020-11-19 17:34:33 +0000

    Change copy to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-  <div style={{ fontWeight: 900, fontSize: 16, lineHeight: '16px' }}>Your</div>
+  <div style={{ fontWeight: 900, fontSize: 16, lineHeight: '16px' }}>Logo</div>
 );
 
 export default Logo;
